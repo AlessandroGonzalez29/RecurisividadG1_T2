e03f41be9f218bcaf34559dacd77d6d51fac7ef1
AlessandroGonzalez29 <25030106@itesa.edu.mx>
2026-09-13 13:53:45 -0600

    Add files via upload
    Subiendo el codigo de c#

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+        public int CalcularFactorial(int num)
+        {
+            // Caso Base
+            if (num==1)
+            {
+                return 1;
+            }
+
+
+
+            //Caso general
+
+            return num = num * CalcularFactorial(num -1); 
+
+
+
+
+        }
+       
+        
+        
+        
+        //public int SumarNumeros(int numero)
+        //{
+        //    //Caso Base
+
+
+        //    int suma;
+        //    suma = numero + num;
+
+        //    //Caso salida
+        //}
+
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Recursividad.Logica;
+
+Console.WriteLine("Ejemplos de recursividad!");
+Console.WriteLine("Factorial !");
+
+Recursividades oRecursividades = new Recursividades();
+
+int resultadoFactorial = oRecursividades.CalcularFactorial(5);
+
+Console.WriteLine("El resultado 5! es " + resultadoFactorial);
+
+Console.WriteLine("Sumar Numeros"); 
